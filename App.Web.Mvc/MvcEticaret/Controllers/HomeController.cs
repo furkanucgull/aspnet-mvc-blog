c80b9f6403cfbd45e61e6afeaea8867b9145be83
furkanucgull <furkanucgull@gmail.com>
2023-06-22 14:23:29 +0300

    Task_7-bitmiş-hali

--- App.Web.Mvc/MvcEticaret/Controllers/HomeController.cs
+++ App.Web.Mvc/MvcEticaret/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MvcEticaret.Models;
+using MvcEticaret.ViewComponents;
 using System.Diagnostics;
 
 namespace MvcEticaret.Controllers
@@ -15,7 +16,85 @@ namespace MvcEticaret.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var productList = new List<ProductItemViewModel>
+        {
+                 new ProductItemViewModel
+            {
+                Title = "Watch 1",
+                StarCount = 2,
+                OldPrice = 99.5m,
+                Price = 79.5m,
+                ImageUrl = "/images/product1.jpg",
+                IsSale = true
+            },
+                 new ProductItemViewModel
+            {
+                Title = "Watch 2",
+                StarCount = 3,
+                OldPrice = 93,
+                Price = 65.59m,
+                ImageUrl = "/images/product2.jpg",
+                IsSale = false
+            },
+                 new ProductItemViewModel
+            {
+                Title = "Watch 3",
+                StarCount = 4,
+                OldPrice = 43,
+                Price = 20,
+                ImageUrl = "/images/product3.jpg",
+                IsSale = true
+            },
+
+                 new ProductItemViewModel
+            {
+                Title = "Watch 4",
+                StarCount = 5,
+                OldPrice = 90,
+                Price = 45,
+                ImageUrl = "/images/product4.jpg",
+                IsSale = false
+            },
+
+                 new ProductItemViewModel
+            {
+                Title = "Watch 5",
+                StarCount = 3,
+                OldPrice = 92,
+                Price = 86.99m,
+                ImageUrl = "/images/product5.jpg",
+                IsSale = false
+            },
+                new ProductItemViewModel
+            {
+                Title = "Watch 6",
+                StarCount = 4,
+                OldPrice = 55,
+                Price = 38.95m,
+                ImageUrl = "/images/product6.jpg",
+                IsSale = false
+            },
+                new ProductItemViewModel
+            {
+                Title = "Watch 7",
+                StarCount = 3,
+                OldPrice = 92,
+                Price = 75,
+                ImageUrl = "/images/product7.1.jpg",
+                IsSale = true
+            },
+                new ProductItemViewModel
+            {
+                Title = "Watch 8",
+                StarCount = 2,
+                OldPrice = 49,
+                Price = 29.5m,
+                ImageUrl = "/images/product8.png",
+                IsSale = false
+            },
+            };
+
+            return View(productList);
         }
 
         public IActionResult Privacy()
---
+++ App.Web.Mvc/MvcEticaret/ViewComponents/ProductItemViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcEticaret.Models;
+using System.Security.Principal;
+
+namespace MvcEticaret.ViewComponents
+{
+    public class ProductItemViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string title, int starCount, decimal oldPrice, decimal price, string imageUrl, bool isSale)
+        {
+            var product = new ProductItemViewModel()
+            {
+                Title = title,
+                Price = price,
+                ImageUrl = imageUrl,
+                IsSale = isSale,
+                OldPrice = oldPrice,
+                StarCount = starCount
+            };
+            return View(product);
+        }
+    }
+}
